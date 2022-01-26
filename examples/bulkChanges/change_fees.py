fa72e6a470933028372663cd12a38b2847191ee1
Karol Sputo <karol.sputo@gmail.com>
2022-01-26 10:07:01 +0100

    Format with Black

--- examples/bulkChanges/change_fees.py
+++ examples/bulkChanges/change_fees.py
@@ -1,11 +1,11 @@
 from BookingSyncApi.api import API
 
+
 def change_fees():
     api = API()
     endpoint = "/rentals?include=rentals_fees"
 
-
-    pages = int(api.get(endpoint).json()['meta']['X-Total-Pages'])
+    pages = int(api.get(endpoint).json()["meta"]["X-Total-Pages"])
     print(pages)
 
     for page in range(1, pages + 1):
@@ -15,12 +15,14 @@ def change_fees():
             if rental["name"][:3] == "KOŁ":
                 print(rental["name"], rental["id"])
                 for fee in rental["rentals_fees"]:
-                    if fee["links"]["fee"] in [29314, 41614] and fee["end_date"] != "2021-09-30":
+                    if (
+                        fee["links"]["fee"] in [29314, 41614]
+                        and fee["end_date"] != "2021-09-30"
+                    ):
                         print(fee["name"]["en"])
 
-    
     # payload = {"rentals_fees" : [{"end_date" : "2021-09-30"}]}
     # print(api.put("/rentals_fees/206734", payload))
 
-change_fees()
 
+change_fees()
