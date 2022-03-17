81c20331a7bccdfee6fc42da6b94a3f6400c3513
Karol Sputo <karol.sputo@gmail.com>
2022-03-17 15:09:38 +0100

    RLH-319 Remove blocking scripts

--- src/bookingsyncapi/tools/blocking_scripts/block_at_18.py
+++
@@ -1,8 +0,0 @@
-from bookingsyncapi.block_rentals import block_rentals
-
-block_rentals("14030", "POZ", 18)
-block_rentals("14030", "KOŁ", 18)
-block_rentals("14030", "GDA", 18)
-block_rentals("14030", "WRO", 18)
-block_rentals("14030", "MIE", 18)
-block_rentals("14030", "ZAK", 18)
--- src/bookingsyncapi/tools/blocking_scripts/block_at_19.py
+++
@@ -1,3 +0,0 @@
-from bookingsyncapi.block_rentals import block_rentals
-
-block_rentals("14030", "MIE", 19)
